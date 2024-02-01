97a94f3b29023c1ce112b7e6c041041291078c12
yeswanthsairaghuram <119839691+yeswanthsairaghuram@users.noreply.github.com>
2024-02-01 11:15:05 +0530

    import java.util.*; public class java{     public static void main(String args []){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         double s=(a+b+c)/2.0;         double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));         System.out.printf("%.2f",area);     } }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class java{
+    public static void main(String args []){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        double s=(a+b+c)/2.0;
+        double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        System.out.printf("%.2f",area);
+    }
+}
