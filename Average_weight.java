b01ca72873421f741344c9587af7124275ef6f3a
yeswanthsairaghuram <119839691+yeswanthsairaghuram@users.noreply.github.com>
2024-02-14 09:47:52 +0530

    import java.util.*; public class Java{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int res;         res=3*x-(y+z);         System.out.println(res);     } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Java{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int res;
+        res=3*x-(y+z);
+        System.out.println(res);
+    }
+}
