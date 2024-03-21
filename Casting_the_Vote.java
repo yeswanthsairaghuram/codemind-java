e7b933d8875d842c1fde7a97ba3d90fc1b5bca84
yeswanthsairaghuram <119839691+yeswanthsairaghuram@users.noreply.github.com>
2024-03-21 14:18:16 +0530

    import java.util.*; public class Vote{     public static void main(String args[]){         Scanner sc= new Scanner(System.in);         int a= sc.nextInt();         if(a>=18){             System.out.println("YES");         }         else {             System.out.println("NO");         }     } }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Vote{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        if(a>=18){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
