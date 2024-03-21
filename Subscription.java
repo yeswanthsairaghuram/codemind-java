2a093d5c002ebbc1d35eb768d58e7a1488709eae
yeswanthsairaghuram <119839691+yeswanthsairaghuram@users.noreply.github.com>
2024-03-21 14:51:41 +0530

    import java.util.*; public class RainFall{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt();         if(a<=40){             System.out.println("NO");         }         else{             System.out.println("YES");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RainFall{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a<=40){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("YES");
+        }
+    }
+}
